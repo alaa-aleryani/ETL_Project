f74dfe0be2a39f91d8b83c8ea4e9614114debe12
hyunyuk <hsyuk0618@gmail.com>
2023-11-15 19:08:27 -0500

    schema made it cleaner

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -1,6 +1,13 @@
 --Look at table
 
+--Contacts Table
 SELECT * from contacts;
+
+--Category Table
 SELECT * from category;
+
+--Subcategory Table
 SELECT * from subcategory;
+
+--Campaign Table
 SELECT * from campaign;
